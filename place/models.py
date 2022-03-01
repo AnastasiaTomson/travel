832f1c77487b0e1e25a28afad36d645967849e7d
Alina Saprykina <alina.saprykina.2002@mail.ru>
2022-03-01 15:51:45 +0700

    Модели мест и маршрутов

--- place/models.py
+++ place/models.py
@@ -3,6 +3,9 @@ from django.db import models
 
 # Картинки
 class Image(models.Model):
+    class Meta:
+        verbose_name = 'Изображение'
+        verbose_name_plural = 'Изображение'
     img = models.ImageField(upload_to="place_image/", verbose_name="Изображение")
     title = models.CharField(max_length=255, verbose_name='Название')
     description = models.TextField(verbose_name='Описание', blank=True, null=True)
@@ -10,33 +13,51 @@ class Image(models.Model):
 
 # Теги
 class Tag(models.Model):
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Тег'
     title = models.CharField(max_length=255, verbose_name='Название')
 
 
 # Города
 class City(models.Model):
+    class Meta:
+        verbose_name = 'Город'
+        verbose_name_plural = 'Город'
     title = models.CharField(max_length=255, verbose_name='Название')
     description = models.TextField(verbose_name='Описание', blank=True, null=True)
 
 
 # Области
 class District(models.Model):
+    class Meta:
+        verbose_name = 'Области'
+        verbose_name_plural = 'Области'
     title = models.CharField(max_length=255, verbose_name='Название')
     description = models.TextField(verbose_name='Описание', blank=True, null=True)
 
 
 # Сезоны
 class Season(models.Model):
+    class Meta:
+        verbose_name = 'Сезоны'
+        verbose_name_plural = 'Сезоны'
     title = models.CharField(max_length=255, verbose_name='Название')
 
 
 # Тип места
 class TypePlace(models.Model):
+    class Meta:
+        verbose_name = 'Тип места'
+        verbose_name_plural = 'Тип места'
     title = models.CharField(max_length=255, verbose_name='Название')
 
 
 # Контакты
 class Contact(models.Model):
+    class Meta:
+        verbose_name = 'Контакты'
+        verbose_name_plural = 'Контакты'
     fb = models.URLField(verbose_name='Фейсбук', blank=True, null=True)
     vk = models.URLField(verbose_name='ВКонтакте', blank=True, null=True)
     inst = models.URLField(verbose_name='Инстаграм', blank=True, null=True)
@@ -50,6 +71,9 @@ class Contact(models.Model):
 
 # Время работы
 class TimeOfWork(models.Model):
+    class Meta:
+        verbose_name = 'Время работы'
+        verbose_name_plural = 'Время работы'
     monday_start = models.TimeField(blank=True, null=True, verbose_name='Начало приема заказов в понедельник')
     monday_end = models.TimeField(blank=True, null=True, verbose_name='Окончание приема заказов в понедельник')
     monday_is_day_off = models.BooleanField(default=False, verbose_name='Понедельник выходной?')
@@ -77,3 +101,22 @@ class TimeOfWork(models.Model):
     sunday_start = models.TimeField(blank=True, null=True, verbose_name='Начало приема заказов в воскресенье')
     sunday_end = models.TimeField(blank=True, null=True, verbose_name='Окончание приема заказов в воскресенье')
     sunday_is_day_off = models.BooleanField(default=False, verbose_name='Воскресенье выходной?')
+
+
+# Место
+class Place(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Место')
+    description = models.TextField(verbose_name='Описание', blank=True, null=True)
+    address = models.TextField(verbose_name='Адрес', blank=True, null=True)
+    coordinates = models.TextField(verbose_name='Координаты')
+    price = models.FloatField(verbose_name='Цена', blank=True, null=True)
+    city = models.ForeignKey(City, verbose_name='Город', on_delete=models.CASCADE, blank=True, null=True)
+    district = models.ForeignKey(District, on_delete=models.CASCADE, verbose_name='Район')
+    season = models.ForeignKey(Season, on_delete=models.CASCADE, verbose_name='Сезон')
+    tag = models.ManyToManyField(Tag, verbose_name='Тэги', balnk=True)
+    type = models.ForeignKey(TypePlace, verbose_name='Тип места', on_delete=models.CASCADE)
+    contact = models.ForeignKey(Contact, on_delete=models.SET_NULL, verbose_name='Контакты', null=True, blank=True)
+    time = models.ForeignKey(TimeOfWork, on_delete=models.SET_NULL, verbose_name='Время работы', null=True, blank=True)
+    image = models.ManyToManyField(Image, verbose_name='Изображения',  blank=True)
+
+# Избранные места
--- trip/models.py
+++ trip/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from users.models import CustomUser
 
-# Create your models here.
+# Маршруты
+class Trip(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Маршрут')
+    description = models.TextField(verbose_name='Описание', blank=True, null=True)
+
+
+# Избранные маршруты
+class FavouriteTrip(models.Model):
+    trip = models.ForeignKey(Trip, on_delete=models.CASCADE, verbose_name='Маршрут')
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, verbose_name='Пользователь')
+
+
+# Пользовательские маршруты
+class UserTrip(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, verbose_name='Пользователь')
+    title = models.CharField(max_length=255, verbose_name='Маршрут')
+    description = models.TextField(verbose_name='Описание', blank=True, null=True)
