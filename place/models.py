34a3aec4ef1743c23f3e348cabf8b2f1a8dcae38
anastasia <anastasia.tomson@mail.ru>
2022-03-01 14:12:34 +0700

    Часть моделей в place + миграции

--- place/models.py
+++ place/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+# Картинки
+class Image(models.Model):
+    img = models.ImageField(upload_to="place_image/", verbose_name="Изображение")
+    title = models.CharField(max_length=255, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание', blank=True, null=True)
+
+
+# Теги
+class Tag(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Название')
+
+
+# Города
+class City(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание', blank=True, null=True)
+
+
+# Области
+class District(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание', blank=True, null=True)
+
+
+# Сезоны
+class Season(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Название')
+
+
+# Тип места
+class TypePlace(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Название')
