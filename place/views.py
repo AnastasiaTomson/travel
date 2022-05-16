15facbbfee013b59a4449683894ff1d2ee7d633f
Alina Saprykina <alina.saprykina.2002@mail.ru>
2022-05-16 13:13:43 +0700

    Рабочие кнопочки и почти готовые плашки туров

--- place/views.py
+++ place/views.py
@@ -1,5 +1,7 @@
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render
+from django.template.loader import render_to_string
+
 from .models import *
 from django.views import View
 from trip.models import *
@@ -9,11 +11,22 @@ from django.http import JsonResponse
 
 
 def index(request):
-    places = Place.objects.order_by("?").filter(image__isnull=False).distinct()[:12]
-    trips = Trip.objects.all().order_by("?")[:3]
-    user_trips = UserTrip.objects.all()
-    user_trip_form = UserTripForm(request_user=request.user)
-    images = Image.objects.all().order_by("?")[:12]
+    title = 'Каталог мест'
+    places = Place.objects.all().order_by("?")[:12]
+    # if request.method == 'POST':
+    #     if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+    #         return JsonResponse({
+    #             "result": True,
+    #             "articles": render_to_string(
+    #                 request=request,
+    #                 template_name='place/index.html',
+    #                 context={
+    #                     'places': get_paginated_page(request, Place.objects.all(), 12)}
+    #             )
+    #         })
+    # return render(request=request, template_name='place/index.html',
+    #               context={'places': get_paginated_page(request, Place.objects.all(), 12),
+    #                         'title': title})
     return render(request, 'place/index.html', locals())
 
 
